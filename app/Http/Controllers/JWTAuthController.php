0453e78377925ef69bd463650dc00da58a599e10
川口博哉 <kawaguchi.hiroya@revamp.co.jp>
2020-01-16 15:58:14 +0900

    ログイン・ログアウト機能の追加実装

--- app/Http/Controllers/JWTAuthController.php
+++ app/Http/Controllers/JWTAuthController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use App\Models\User;
 use App\Http\Requests\JwtAuthRequest;
+use App\Http\Requests\JwtLoginRequest;
+use Auth;
 
 class JWTAuthController extends Controller
 {
@@ -19,6 +21,44 @@ class JWTAuthController extends Controller
         return response()->json([
             'status' => 'success',
             'data' => $user
+        ], 200);
+    }
+
+    public function login(JwtLoginRequest $request) {
+
+        if(!$token = Auth::guard('api')->attempt(['email' => $request->email, 'password' => $request->password])) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        return $this->respondWithToken($token);
+    }
+
+    protected function respondWithToken($token) {
+    
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => Auth::guard('api')->factory()->getTTL() * 60
+        ]);
+    }
+
+    public function user() {
+
+        return response()->json(Auth::guard('api')->user());
+    }
+
+    public function refresh() {
+    
+        return $this->respondWithToken(Auth::guard('api')->refresh());
+    }
+
+    public function logout() {
+    
+        Auth::guard('api')->logout();
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'logout'
         ], 200);
     }
 }
